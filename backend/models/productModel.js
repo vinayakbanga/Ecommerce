9d514c30778c3da5cceb485297ec2371a2f01a2d
Vinayak Banga <vinayakbanga22@gmail.com>
2022-07-17 02:04:27 +0530

    write and update review for a product route created

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -18,6 +18,11 @@ const productSchema=new mongoose.Schema({
         name:{type:String,required:true},
         rating:{type:Number,required:true},
         comment:{type:String},
+        user:{
+            type:mongoose.Schema.ObjectId,
+            ref:"User",
+            required:true
+        }
     }],
 
     user:{
